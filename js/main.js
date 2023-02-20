6677d20129d7aeb3d358b2b4d7202db5e7008787
Samuel Almeida <88410126+SamuelWSGIT@users.noreply.github.com>
2023-02-20 18:41:24 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,51 +1,56 @@
-//recebendo formulario do html
-const form = document.querySelector("#novoItem")
-//recebendo lista do html
-const lista = document.querySelector("#lista")
-//recebendo arrei de itens do local storage se existir
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.querySelector("#novoItem"); //recebendo formulario do html
+const lista = document.querySelector("#lista"); //recebendo lista do html
+const itens = JSON.parse(localStorage.getItem("itens")) || []; //recebendo arrei de itens do local storage se existir
+
 //pra cara item dentro do array do local storage, executar a função criar elemento com os elementos dentro do array
-itens.forEach(element => {
-    criaElemento(element)
+itens.forEach((element) => {
+  criaElemento(element);
 });
+
 //adicionando leitor de evento do tipo submit
-form.addEventListener("submit", (evento)=>{
-    //removendo propriedades padão do evento
-    evento.preventDefault()
-    //pegando o nome do elemento
-    const nome = evento.target.elements['nome']
-    //pegando quantidade do elemento
-    const quantidade = evento.target.elements['quantidade']
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault(); //removendo propriedades padão do evento
+  const nome = evento.target.elements["nome"]; //pegando o nome do elemento
+  const quantidade = evento.target.elements["quantidade"]; //pegando quantidade do elemento
+
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
+
+  const itemAtual = {
     //criando array com valor do nome e quantidade atuais
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
-    //executar função criar elemento com o array atual
-    criaElemento(itemAtual)
-    //inserindo o array atual dentro da lista de arrays
-    itens.push(itemAtual)
-    //enviando a lista de itens para o localstorage com o nome de itens
-    localStorage.setItem("itens", JSON.stringify(itens))
-    //removendo o nome inserido no input
-    nome.value = ""
-    //removendo a quantidade inserida no input
-    quantidade.value = ""
-})
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  if (existe) {
+    itemAtual.id = existe.id;
+
+    atualizaElemento(itemAtual);
+  } else {
+    itemAtual.id = itens.length;
+    criaElemento(itemAtual); //executar função criar elemento com o array atual
+    itens.push(itemAtual); //inserindo o array atual dentro da lista de arrays
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens)); //enviando a lista de itens para o localstorage com o nome de itens
+
+  nome.value = ""; //removendo o nome inserido no input
+  quantidade.value = ""; //removendo a quantidade inserida no input
+});
+
 //função para criar elementos no html
 function criaElemento(item) {
-    //variavel que cria li
-    const novoItem = document.createElement('li')
-    //adicionando classe na li
-    novoItem.classList.add("item")
-    //variavel que cria strong
-    const numeroItem = document.createElement('strong')
-    //inserindo html na variavel strong com os dados da quantidade atual do input
-    numeroItem.innerHTML = item.quantidade
-    //inserindo variavel strong dentro da variavel li
-    novoItem.appendChild(numeroItem)
-    //adicionando li com a variavel nome do item atual
-    novoItem.innerHTML += item.nome
-    //adicionando li dentro da lista
-    lista.appendChild(novoItem)
+  const novoItem = document.createElement("li"); //variavel que cria li
+  novoItem.classList.add("item"); //adicionando classe na li
+
+  const numeroItem = document.createElement("strong"); //variavel que cria strong
+  numeroItem.innerHTML = item.quantidade; //inserindo html na variavel strong com os dados da quantidade atual do input
+  novoItem.appendChild(numeroItem); //inserindo variavel strong dentro da variavel li
+
+  numeroItem.dataset.id = item.id;
+  novoItem.innerHTML += item.nome; //adicionando li com a variavel nome do item atual
+  lista.appendChild(novoItem); //adicionando li dentro da lista
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
